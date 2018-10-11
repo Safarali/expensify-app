e1b71b74d4b1fcc99269c13d13ed8c728ee44cc2
Safarali <mr.safarali@gmail.com>
2018-10-11 15:03:51 -0700

    Add Enzyme and enzyme-to-json to test Components

---
+++ src/tests/components/Header.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Header from '../../components/Header';
+
+describe('<Header />', () => {
+    test('should render Header component', () => {
+        const wrapper = shallow(<Header/>);
+        expect(wrapper).toMatchSnapshot();
+        // const renderer = new ReactShallowRenderer();
+        // renderer.render(<Header />);
+        // expect(renderer.getRenderOutput()).toMatchSnapshot();
+    });
+});
