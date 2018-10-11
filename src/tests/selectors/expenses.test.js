1c68481a4642465098990aaf882a77b3746c6d05
Safarali <mr.safarali@gmail.com>
2018-10-10 17:10:19 -0700

    Remove Expenses, import from fixtures

--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -1,27 +1,6 @@
-import { getVisibleExpenses as selectExpenses} from '../../selectors/expenses';
 import moment from 'moment';
-const expenses = [
-    {
-        id: '1',
-        description: 'Rent',
-        amount: 500,
-        note: '',
-        createdAt: 0
-    },{
-        id: '2',
-        description: 'Rental Insurance',
-        amount: 600,
-        note: '',
-        createdAt: moment(0).subtract(4, 'days').valueOf()
-    },{
-        id: '3',
-        description: 'Car payment',
-        amount: 7000,
-        note: '',
-        createdAt: moment(0).add(4, 'days').valueOf()
-    }
-];
-
+import { getVisibleExpenses as selectExpenses} from '../../selectors/expenses';
+import expenses from '../fixtures/expenses';
 const defaultFilters = {
     text: '',
     sortBy: 'date',
