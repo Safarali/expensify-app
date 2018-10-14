3e5918fbb484838ba2cd4a54403bf8a7fbeba332
Safarali <mr.safarali@gmail.com>
2018-10-13 21:10:54 -0700

    Finish ExpenseForm testing

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 import { shallow } from 'enzyme';
 import ExpenseForm from '../../components/ExpenseForm';
 import expenses from '../fixtures/expenses';
@@ -23,6 +24,22 @@ describe('<ExpenseForm/>', () => {
             });
             expect(wrapper.state('error').length).toBeGreaterThan(0);
             expect(wrapper).toMatchSnapshot();
+        });
+
+        test('should call onSubmit prop for valid form submission', () => {
+            const onSubmitSpy = jest.fn();
+            const wrapper = shallow(<ExpenseForm expense={expenses[0]} onSubmit={onSubmitSpy}/>);
+            wrapper.find('form').simulate('submit', {
+                preventDefault: () => {}
+            });
+
+            expect(wrapper.state('error')).toBe('');
+            expect(onSubmitSpy).toHaveBeenLastCalledWith({
+                description: expenses[0].description,
+                amount: expenses[0].amount,
+                note: expenses[0].note,
+                createdAt: expenses[0].createdAt
+            });
         });
     });
 
@@ -63,6 +80,20 @@ describe('<ExpenseForm/>', () => {
                 target: { value }
             });
             expect(wrapper.state('amount')).not.toBe(value);
+        });
+
+        test('should set a new date on date change', () => {
+            const now = moment();
+            const wrapper = shallow(<ExpenseForm/>);
+            wrapper.find('withStyles(SingleDatePicker)').prop('onDateChange')(now);
+            expect(wrapper.state('createdAt')).toEqual(now);
+        });
+
+        test('should set calendar focus on change', () => {
+            const focused = true;
+            const wrapper = shallow(<ExpenseForm/>);
+            wrapper.find('withStyles(SingleDatePicker)').prop('onFocusChange')({ focused });
+            expect(wrapper.state('calendarFocused')).toBe(focused);
         });
     });
 });
