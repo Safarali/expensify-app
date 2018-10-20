0d0dbfc4cd3a104135cc0c6f67ce90a2216fb145
Safarali <mr.safarali@gmail.com>
2018-10-19 23:31:33 -0700

    Orginize and wipe out the dummy data setup

--- src/app.js
+++ src/app.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import AppRouter from './Routers/AppRouters';
 import { Provider } from 'react-redux';
 import { store } from './store/configStore';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
