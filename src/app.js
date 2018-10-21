30207dc5c8ab2d4a5d94a531318165f51f7a2e31
Safarali <mr.safarali@gmail.com>
2018-10-21 13:13:08 -0700

    Render app once data successfully fetched

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import AppRouter from './Routers/AppRouters';
 import { Provider } from 'react-redux';
 import { store } from './store/configStore';
+import { startSetExpenses } from './actions/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
@@ -12,4 +13,7 @@ const jsx = (
         <AppRouter/>
     </Provider>
 );
-ReactDOM.render(jsx, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render(jsx, document.getElementById('app'));
+});
