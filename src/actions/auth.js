1573e8ed15c36555b4789879e0beb73a427a97eb
Safarali <mr.safarali@gmail.com>
2018-10-24 15:43:14 -0700

    Add google authentication

---
+++ src/actions/auth.js
@@ -0,0 +1,7 @@
+import { googleAuthProvider, firebase } from "../firebase/firebase";
+
+export const startLogin = () => {
+    return () => {
+        return firebase.auth().signInWithPopup(googleAuthProvider);
+    };
+}
