47db028bcca2bd45ba7c760ae0b9eda5395cf8b2
Safarali <mr.safarali@gmail.com>
2018-10-25 14:35:10 -0700

    Set up private routes using HOC

--- src/Routers/AppRouters.js
+++ src/Routers/AppRouters.js
@@ -1,24 +1,23 @@
 import React, { Fragment } from 'react';
 import { Router, Route, Switch, Link, NavLink } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory'
-import Header from '../components/Header';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import LoginPage from '../components/LoginPage';
 import NotFoundPage from '../components/NotFoundPage';
+import PrivateRoute from './PrivateRoute';
 
 export const history = createHistory();
 
 const AppRouter = () => (
     <Router history={history}>
         <Fragment>
-            <Header/>
             <Switch>
                 <Route exact={true} path='/' component={LoginPage} />
-                <Route path="/dashboard" component={ExpenseDashboardPage} />
-                <Route path="/create" component={AddExpensePage} />
-                <Route path="/edit/:id" component={EditExpensePage} />
+                <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
+                <PrivateRoute path="/create" component={AddExpensePage} />
+                <PrivateRoute path="/edit/:id" component={EditExpensePage} />
                 <Route component={NotFoundPage} />
             </Switch>
         </Fragment>
