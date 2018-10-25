5207d7fbb98349af73e4a76c9a738249bc198bdc
Safarali <mr.safarali@gmail.com>
2018-10-25 01:37:02 -0700

    Fix typo

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -2,7 +2,7 @@ const authReducer = (state = {}, action) => {
     switch (action.type) {
         case 'LOG_IN':
             return {
-                uid: action.uid,
+                uid: action.uid
             };
         case 'LOG_OUT':
             return {};
