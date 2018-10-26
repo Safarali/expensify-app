537940ea3a74d55150549073e336ceb2f17e077b
Safarali <mr.safarali@gmail.com>
2018-10-26 00:34:29 -0700

    Style LoginPage

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,9 +3,13 @@ import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 
 export const LoginPage = ({ startLogin }) => (
-    <div>
-        <button onClick={startLogin}>Login</button>
-    </div>
+    <header className="header">
+        <div className="header__box">
+            <h1 className="header__title">Expensify App</h1>
+            <p>It's time to get your expenses under control.</p>
+            <button onClick={startLogin}>Login</button>
+        </div>
+    </header>
 );
 
 const mapDispatchToProps = (dispatch) => ({
@@ -13,4 +17,3 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 export default connect(undefined, mapDispatchToProps)(LoginPage);
-
